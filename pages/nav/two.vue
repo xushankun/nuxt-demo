abb57b1b0256e44fa7fea7094ba39300ecb8da0b
xushankun <lovexsk@qq.com>
2019-08-07 09:58:47 +0800

    优化

--- pages/nav/two.vue
+++ pages/nav/two.vue
@@ -2,7 +2,6 @@
     <div>
       <h2>nav/two的内容</h2>
       <div>花开花落</div>
-      <div>{{ baseUrl }}</div>
 <!--      <div>host:{{ host }}</div>-->
     </div>
 </template>
@@ -11,9 +10,6 @@
     export default {
         name: "two.vue",
         async asyncData (content) {
-          return {
-            baseUrl: process.env.npm_package_baseUrl || 'localhost:xxx'
-          }
           // console.log(content.req)
           // if (process.server) {
           //   return { host: content.req.headers.host || '没有。。' }
