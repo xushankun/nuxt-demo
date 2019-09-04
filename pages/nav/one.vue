eec83e4517cb1b6078f22883d3a9f2c5cbeaadd3
xushankun <lovexsk@qq.com>
2019-09-04 16:16:55 +0800

    优化

--- pages/nav/one.vue
+++ pages/nav/one.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-      <div>host:{{ host }}</div>
       <h2>新闻列表</h2>
       <ul>
         <li v-for="(item,index) in list" :key="index" v-if="index <= 2">
@@ -23,14 +22,11 @@
           // ctx.query  // 路由问号后面的参数
           // ctx.error   // 错误处理方法
           // process.server  为true 时表示当前服务端【首次渲染为true】
-          let _host = process.server ? ctx.req.headers.host : ''
-          ctx.app.printHost(_host)
           ctx.app.$ctxVueInjected('asyncData:$ctxVueInjected')
           let res = await axios.get(`http://localhost:3000/api/news`)
           console.log(res.data.data)
           return {
-            list: res.data.data,
-            host: _host
+            list: res.data.data
           }
         },
       created() {
